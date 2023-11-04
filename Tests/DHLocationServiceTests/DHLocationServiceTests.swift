3b62965b33d10caec158b1dff217fd33187e9463
David Hu <davidhu.sg66@gmail.com>
2023-11-04 11:59:21 +0800

    [feature] add main location functions

--- Tests/DHLocationServiceTests/DHLocationServiceTests.swift
+++ Tests/DHLocationServiceTests/DHLocationServiceTests.swift
@@ -3,9 +3,7 @@ import XCTest
 
 final class DHLocationServiceTests: XCTestCase {
     func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(DHLocationService().text, "Hello, World!")
+        // TODO: Test cases later
+//        XCTAssertEqual(DHLocationService().text, "Hello, World!")
     }
 }
